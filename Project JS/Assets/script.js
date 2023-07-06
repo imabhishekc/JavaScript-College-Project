e876a1e0664e3fbe60cdf68c5528d5a8ca1c9cf5
Your Name <karkialson321@gmail.com>
2023-07-06 10:17:11 +0545

    landing-pagechanges

--- Project JS/Assets/script.js
+++ Project JS/Assets/script.js
@@ -1,5 +1,5 @@
-const arrowIcon = document.querySelector('.material-symbols-outlined');
 
+const arrowIcon = document.querySelector('.material-symbols-outlined');
 // Add event listener for mouseenter event
 arrowIcon.addEventListener('mouseenter', () => {
   arrowIcon.style.transform = 'rotate(180deg)';
@@ -9,3 +9,19 @@ arrowIcon.addEventListener('mouseenter', () => {
 arrowIcon.addEventListener('mouseleave', () => {
   arrowIcon.style.transform = 'rotate(0deg)';
 });
+
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', (e) => {
+  cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+});
+
+document.addEventListener('mouseenter', () => {
+  cursor.style.transform = 'translate(-50%, -50%)';
+});
+
+document.addEventListener('mouseleave', () => {
+  cursor.style.transform = 'translate(-9999px, -9999px)';
+});
+
